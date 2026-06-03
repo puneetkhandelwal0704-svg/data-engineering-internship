62bbaf3d0ff22865748d3dcf42d951435ecfe577
hello-world <puneetkhandelwal0704@gmail.com>
2026-06-03 22:04:42 +0530

    added day8 work

--- Pandas/day8/greet.py
+++ Pandas/day8/greet.py
@@ -1,3 +1,2 @@
 def greeting(name):
     print("hello",name)
-    
--- Pandas/day8/pymath.py
+++ Pandas/day8/pymath.py
@@ -1,11 +1,7 @@
-***def sum(n1, n2):
+'''def sum(n1, n2):
     return n1 + n2
 
 def null():
-    pass***
+    pass'''
 
 
-
-
-
-    
